8f786c1398e3c9ae5424ba661710d82c0f91e826
Bhumihar Sachin <bhumiharsachin0@gmail.com>
2023-03-20 20:12:20 +0530

    logging and exception

--- setup.py
+++ setup.py
@@ -3,6 +3,8 @@ from gettext import install
 from setuptools import find_packages, setup
 from typing import List
 
+
+HYPEN_E_DOT = '-e .'
 def get_requirements(file_path:str)->List[str]:
     """
     This function will return list of requirements
@@ -11,8 +13,10 @@ def get_requirements(file_path:str)->List[str]:
     with open(file_path) as file_obj:
         requirements = file_obj.readlines()
         requirements = [req.replace("\n", "") for req in requirements]
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
 
-
+    return requirements
 
 setup(
 name = 'End-to-End ML Project',
@@ -21,9 +25,5 @@ author = 'Sachin Bhumihar',
 author_email= 'bhumiharsachin0@gmail.com',
 packages= find_packages(),
 install_requires = get_requirements('requirements.txt')
-
-
-
-
-    
+   
 )
